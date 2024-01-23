494743f4710799efdb6f9007a938b05dfe18e2e3
areeshalam <areeshpersonal5@gmail.com>
2024-01-23 13:05:47 +1100

    23 Jan 2024 Refactored Folders

--- routes/New folder/viewsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const viewsController = require('../controllers/viewsController');
-// const authController = require('../controllers/authController');
-
-const router = express.Router();
-
-router.get('/', viewsController.getOverview);
-
-module.exports = router;
